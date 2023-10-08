8bf590b125f82395cb2ff12fee2f222451a9fe7a
Vikas Yadav <91658308+Vikasyadav25@users.noreply.github.com>
2023-10-08 17:31:18 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,21 @@
 // connected to database
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://vikassinghyadav6:ot9cfaHIUpqEab5U@cluster1.9oquvxy.mongodb.net/Issues_Tracker`);
+// const mongoose = require('mongoose');
+// mongoose.connect(`mongodb+srv://vikassinghyadav6:ot9cfaHIUpqEab5U@cluster1.9oquvxy.mongodb.net/Issues_Tracker`);
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, "Error connecting to mongodb"));
-db.once('open', function(){
-    console.log("Connected to Database :: mongodb");
-});
-module.exports = db;
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, "Error connecting to mongodb"));
+// db.once('open', function(){
+//     console.log("Connected to Database :: mongodb");
+// });
+// module.exports = db;
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/'
+
+async function getEmpData() {
+    let client = await MongoClient.connect(url);
+    let connection = client.db('PROJECT1_ISSUESTACKER'); // Establish connection
+    return connection.collection('Wipro'); // creating a collection and naming it also
+}
+
+module.exports = getEmpData;
