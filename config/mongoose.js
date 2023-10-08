30658035764fbe852edf7005b56e3725b56e80c6
Vikas Yadav <91658308+Vikasyadav25@users.noreply.github.com>
2023-10-08 16:07:03 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+// connected to database
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb+srv://vikassinghyadav6:ot9cfaHIUpqEab5U@cluster1.9oquvxy.mongodb.net/Issues_Tracker`);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "Error connecting to mongodb"));
+db.once('open', function(){
+    console.log("Connected to Database :: mongodb");
+});
+module.exports = db;
